eff648887c8931d5b2b3607c5eb47723acf65a1c
Morice-Gallier Nikola <morice-galliernikola@MacBook-Pro-de-Morice-Gallier.local>
2025-04-22 13:07:39 +0200

    rework page size

--- app/_use_cases/Hexacodle/ui_components/Hexacodle.tsx
+++ app/_use_cases/Hexacodle/ui_components/Hexacodle.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Header from "@/app/_cross_project/ui_components/Header";
 import { useHexacodle } from "@/app/_use_cases/Hexacodle/domain/useHexacodle";
 import ColoredSquare from "@/app/_use_cases/Hexacodle/ui_components/ColoredSquare";
 import CurrentGuess from "@/app/_use_cases/Hexacodle/ui_components/CurrentGuess";
@@ -24,8 +23,7 @@ const Hexacodle = () => {
   } = useHexacodle();
 
   return (
-    <div className="flex flex-col h-full">
-      <Header />
+    <>
       <div className="flex flex-col items-center justify-between gap-5 h-full pt-3">
         <div className="flex gap-5">
           <ColoredSquare hexColor={targetHexColor} label="Target" />
@@ -48,7 +46,7 @@ const Hexacodle = () => {
       <Modal isOpen={isOpen} modalRef={modalRef} className="h-2/3 w-full">
         <EmojiGuesses finalGuesses={guessedHexColors} />
       </Modal>
-    </div>
+    </>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Header from "@/app/_cross_project/ui_components/Header";
 import Hexacodle from "@/app/_use_cases/Hexacodle/ui_components/Hexacodle";
 
 export default function Home() {
   return (
-    <div className="h-dvh">
-      <Hexacodle />
-    </div>
+    <>
+      <Header />
+      <div className="h-[calc(100dvh-48px)]">
+        <Hexacodle />
+      </div>
+    </>
   );
 }
