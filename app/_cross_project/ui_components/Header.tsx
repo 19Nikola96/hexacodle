3b5285b1baa90714e95534da1053c63e81266add
Morice-Gallier Nikola <morice-galliernikola@MacBook-Pro-de-Morice-Gallier.local>
2025-04-22 13:47:25 +0200

    add menu display and animation

--- app/_cross_project/ui_components/Header.tsx
+++ app/_cross_project/ui_components/Header.tsx
@@ -1,15 +1,53 @@
 "use client";
 
+import {
+  Modal,
+  ModalAnimation,
+} from "@/app/reusable_in_other_projetcs/Modal/Modal";
+import { useModal } from "@/app/reusable_in_other_projetcs/Modal/useModal";
 import Hamburger from "hamburger-react";
-import { useState } from "react";
 
 const Header = () => {
-  const [isOpen, setOpen] = useState(false);
+  const { isOpen, openModal, closeModal, modalRef } = useModal();
 
   return (
-    <div className="flex justify-end">
-      <Hamburger toggled={isOpen} toggle={setOpen} rounded size={20} />
-    </div>
+    <>
+      <div className="flex justify-end z-10">
+        <Hamburger
+          toggled={isOpen}
+          onToggle={(toggled) => {
+            if (toggled) {
+              openModal();
+            } else {
+              closeModal();
+            }
+          }}
+          rounded
+          size={24}
+        />
+      </div>
+      <Modal
+        modalRef={modalRef}
+        isOpen={isOpen}
+        className="h-full w-full rounded-none p-0"
+        customModalAnimation={ModalAnimation.menu}
+      >
+        <div className="flex justify-end z-10">
+          <Hamburger
+            toggled={isOpen}
+            onToggle={(toggled) => {
+              if (toggled) {
+                openModal();
+              } else {
+                closeModal();
+              }
+            }}
+            rounded
+            size={24}
+          />
+        </div>
+      </Modal>
+    </>
   );
 };
 
