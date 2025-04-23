82a77bd4a762682b1c72c5b5df3bd10c53c6aaa5
Morice-Gallier Nikola <morice-galliernikola@MacBook-Pro-de-Morice-Gallier.local>
2025-04-23 08:55:03 +0200

    try prevent cached target square color

--- app/_use_cases/Hexacodle/ui_components/ColoredSquare.tsx
+++ app/_use_cases/Hexacodle/ui_components/ColoredSquare.tsx
@@ -22,6 +22,7 @@ const ColoredSquare = ({
       <div className="w-36 h-36 rounded-lg neomorphism border-4 border-white relative overflow-hidden">
         <div
           className="translate-in-animation w-full h-full"
+          id={`colored-square-${new Date().toISOString()}`}
           style={{ backgroundColor: hexColor ?? BASED_GUESS_HEX_COLOR }}
         />
         {guessedHexColors &&
