ebf5d0cdda7618b34e31ff76cc99cce0043e0523
Morice-Gallier Nikola <morice-galliernikola@MBPdeMoeGallier.lan>
2025-04-26 13:41:19 +0200

    change dark animation position and fade in square

--- app/_use_cases/Hexacodle/ui_components/ColoredSquare.tsx
+++ app/_use_cases/Hexacodle/ui_components/ColoredSquare.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { Guess } from "@/app/_use_cases/Hexacodle/domain/constants";
 import { transformGuessIntoHexaString } from "@/app/_use_cases/Hexacodle/domain/transformGuessIntoHexaString";
 
@@ -17,20 +16,15 @@ const ColoredSquare = ({
   label,
   guessedHexColors,
 }: ColoredSquareProps) => {
-  const [dynamicId, setDynamicId] = useState("colored-square");
-
-  useEffect(() => {
-    // Update the id on the client side to prevent caching
-    setDynamicId(`colored-square-${new Date().toISOString()}`);
-  }, []);
-
   return (
-    <div className="flex flex-col items-center justify-center gap-2">
+    <div
+      className="flex flex-col items-center justify-center gap-2 fade-in-animation"
+      style={{ animationDelay: hexColor ? "0ms" : "200ms" }}
+    >
       <span className="dark:text-white">{label}</span>
       <div className="w-38 h-38 rounded-lg neomorphism border-4 border-white relative overflow-hidden transition-all">
         <div
           className="translate-in-animation w-full h-full"
-          id={dynamicId}
           style={{ backgroundColor: hexColor ?? BASED_GUESS_HEX_COLOR }}
         />
         {guessedHexColors &&
